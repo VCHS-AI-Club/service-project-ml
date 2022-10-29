b252c93374232848988b66558659d5455221aeb0
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-10-29 14:29:15 -0700

    preliminary suggestion alg complete with testing data

--- ml/suggestion_algorithm.py
+++ ml/suggestion_algorithm.py
@@ -1,43 +1,46 @@
 from typing import Dict, Text
 import tensorflow as tf
+
 import numpy as np
-
-
 import tensorflow_recommenders as tfrs
-import tensorflow_datasets as tfds
 
-# Ratings data.
-ratings = tfds.load('movielens/100k-ratings', split="train")
-# Features of all the available movies.
-movies = tfds.load('movielens/100k-movies', split="train")
+ratings = np.random.randint(10, size=(100))
+users = np.random.randint(100, size=(100))
+
+opp = np.array([])
+for i in range(100):
+  opp = np.append(opp, "some text{}".format(i))
+
+ratings_dataset = {i:j for i, j in zip(opp, users)}
 
 # Select the basic features.
-ratings = ratings.map(lambda x: {
-    "movie_title": x["movie_title"],
-    "user_id": x["user_id"]
-})
-movies = movies.map(lambda x: x["movie_title"])
+# ratings = ratings.map(lambda x: {
+    # "movie_title": x["movie_title"],
+    # "user_id": x["user_id"]
+# })
 
-user_ids_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
-user_ids_vocabulary.adapt(ratings.map(lambda x: x["user_id"]))
+# movies = movies.map(lambda x: x["movie_title"])
 
-movie_titles_vocabulary = tf.keras.layers.StringLookup(mask_token=None)
-movie_titles_vocabulary.adapt(movies)
+user_id_vocab = tf.keras.layers.StringLookup(mask_token=None)
+user_id_vocab.adapt(ratings_dataset.keys())
 
-class MovieLensModel(tfrs.Model):
+opp_titles_vocab = tf.keras.layers.StringLookup(mask_token=None)
+opp_titles_vocab.adapt(opp)
+
+class OpportunityModel(tfrs.Model):
   # We derive from a custom base class to help reduce boilerplate. Under the hood,
   # these are still plain Keras Models.
 
   def __init__(
       self,
       user_model: tf.keras.Model,
-      movie_model: tf.keras.Model,
+      opp_model: tf.keras.Model,
       task: tfrs.tasks.Retrieval):
     super().__init__()
 
     # Set up user and movie representations.
     self.user_model = user_model
-    self.movie_model = movie_model
+    self.opp_model = opp_model
 
     # Set up a retrieval task.
     self.task = task
@@ -46,30 +49,28 @@ class MovieLensModel(tfrs.Model):
     # Define how the loss is computed.
 
     user_embeddings = self.user_model(features["user_id"])
-    movie_embeddings = self.movie_model(features["movie_title"])
+    opp_embeddings = self.movie_model(features["opp"])
 
-    return self.task(user_embeddings, movie_embeddings)
+    return self.task(user_embeddings, opp_embeddings)
 
-embedding_size = 64
-batch_size = 128
+embedding_size = 32 
+batch_size = 10 
 
-# Define user and movie models.
 user_model = tf.keras.Sequential([
-    user_ids_vocabulary,
-    tf.keras.layers.Embedding(user_ids_vocabulary.vocabulary_size(), embedding_size)
+    user_id_vocab,
+    tf.keras.layers.Embedding(user_id_vocab.vocabulary_size(), embedding_size)
 ])
-movie_model = tf.keras.Sequential([
-    movie_titles_vocabulary,
-    tf.keras.layers.Embedding(movie_titles_vocabulary.vocabulary_size(), embedding_size)
+opp_model = tf.keras.Sequential([
+    opp_titles_vocab,
+    tf.keras.layers.Embedding(opp_titles_vocab.vocabulary_size(), embedding_size)
 ])
 
-# Define your objectives.
 task = tfrs.tasks.Retrieval(metrics=tfrs.metrics.FactorizedTopK(
-    movies.batch(batch_size).map(movie_model)
+    opp.batch(batch_size).map(opp_model)
   )
 )
-# Create a retrieval model.
-model = MovieLensModel(user_model, movie_model, task)
+
+model = OpportunityModel(user_model, opp_model, task)
 model.compile(optimizer=tf.keras.optimizers.Adagrad(0.5))
 
 # Train for 3 epochs.
@@ -78,7 +79,8 @@ model.fit(ratings.batch(4096), epochs=3)
 # Use brute-force search to set up retrieval using the trained representations.
 index = tfrs.layers.factorized_top_k.BruteForce(model.user_model)
 index.index_from_dataset(
-    movies.batch(100).map(lambda title: (title, model.movie_model(title))))
+    opp.batch(100).map(lambda title: (title, model.opp_model(title))))
+    
 
 # Get some recommendations.
 _, titles = index(np.array(["42"]))
